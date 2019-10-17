b80e7d53f75ecc97ffbabc0e8e0c0fa9b8a07b57
asiris3 <amornthep.sirisaengchaikul@exxonmobil.com>
2019-10-17 14:55:08 +0800

    [kafka-learn] [Byte] -kafka send json

--- src/main/java/com/linux/huhx/kafka/controller/KafkaController.java
+++ src/main/java/com/linux/huhx/kafka/controller/KafkaController.java
@@ -1,10 +1,13 @@
 package com.linux.huhx.kafka.controller;
 
+import com.linux.huhx.kafka.domain.Customer;
 import com.linux.huhx.kafka.domain.KafkaMessage;
 import com.linux.huhx.kafka.service.ProducerService;
 import javax.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +21,10 @@ public class KafkaController {
   @GetMapping("/{key}/{message}")
   public KafkaMessage sendKafkaMessage(@PathVariable("key") String key, @PathVariable("message") String message) {
     return producerService.sendMessage(key, message);
+  }
+
+  @PostMapping("/publish")
+  public KafkaMessage sendKafkaMessageObject(@RequestBody Customer customer) {
+    return producerService.sendObject(customer);
   }
 }
--- src/main/java/com/linux/huhx/kafka/service/ProducerService.java
+++ src/main/java/com/linux/huhx/kafka/service/ProducerService.java
@@ -1,10 +1,12 @@
 package com.linux.huhx.kafka.service;
 
+import com.linux.huhx.kafka.domain.Customer;
 import com.linux.huhx.kafka.domain.KafkaMessage;
 import com.linux.huhx.kafka.message.Message;
 import java.util.Date;
 import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
@@ -18,11 +20,25 @@ public class ProducerService {
   @Resource
   private KafkaTemplate<String, KafkaMessage> kafkaTemplate;
 
+  @Autowired
+  private KafkaTemplate<String, KafkaMessage> kafkaTemplateObject;
+
   public KafkaMessage sendMessage(String key, String message) {
     log.info(String.format("#### -> Producing message -> %s", message));
     KafkaMessage<Message> kafkaMessage = new KafkaMessage(new Message(message, "byte"), new Date());
     try {
       SendResult<String, KafkaMessage> sendResult = this.kafkaTemplate.send(TOPIC, key, kafkaMessage).get();
+      log.info("send result = {}", sendResult.toString());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    return kafkaMessage;
+  }
+
+  public KafkaMessage sendObject(Customer customer) {
+    KafkaMessage<Message> kafkaMessage = new KafkaMessage(customer.toString(), new Date());
+    try {
+      SendResult<String, KafkaMessage> sendResult = this.kafkaTemplateObject.send(TOPIC, "1", kafkaMessage).get();
       log.info("send result = {}", sendResult.toString());
     } catch (Exception e) {
       e.printStackTrace();
